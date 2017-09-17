1b5aa69ea7bfeb87f1be1102ea20feb62b4b27a1
Justin Ho <justinhodev@gmail.com>
2017-09-17 00:07:43 -0700

    setup vue js / nuxt js / bootstrap

---
+++ site/pages/index.vue
@@ -0,0 +1,46 @@
+<template>
+  <section class="container">
+    <b-jumbotron header="Neur-ish" lead="Satisfying mental health with food">
+      <p>For more information visit website</p>
+      <b-btn variant="primary" to="mood-selector">Select Mood</b-btn>
+    </b-jumbotron>
+  </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+  /* 1 */
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
---
+++ site/pages/mood-selector.vue
@@ -0,0 +1,37 @@
+<template>
+  <b-container fluid>
+  <b-container fluid>
+      <b-row>
+        <b-col cols="4">
+          <b-button variant="primary" to="/mood-selector/happy">happy</b-button>
+        </b-col>
+        <b-col cols="4">
+            <b-button variant="primary" to="/mood-selector/balanced">balanced</b-button>
+        </b-col>
+        <b-col cols="4">
+            <b-button variant="primary" to="/mood-selector/relaxed">relaxed</b-button>
+        </b-col>
+      </b-row>
+      <b-row>
+          <b-col cols="4">
+            <b-button variant="primary" to="/mood-selector/energized">energized</b-button>
+          </b-col>
+          <b-col cols="4">
+             <b-button variant="primary" to="/mood-selector/focused">focused</b-button>
+          </b-col>
+          <b-col cols="4">
+            <b-button variant="primary" to="/mood-selector/awake">awake</b-button>
+          </b-col>
+      </b-row>
+  </b-container>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
